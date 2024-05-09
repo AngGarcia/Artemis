659860d01846a5ba94abdfb9a04c8233e1123527
EliiStark <elisaalonsosa@gmail.com>
2024-05-09 11:13:54 +0200

    General (#1)
    -Modificado el orden de las escenas en el proyecto

--- Assets/Aplicaciones/General/Scripts/ChangeScene.cs
+++ Assets/Aplicaciones/General/Scripts/ChangeScene.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    
     public void CambiarEscena(int indiceEscena)
     {
         SceneManager.LoadScene(indiceEscena);
